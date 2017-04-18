79c4bc3997cb418d8546b48bec06c75d98ab58fe
omnilance <github@omnilance.com>
2017-04-18 12:07:22 +0300

    Update to v2.0

--- api.php
+++ api.php
@@ -1,351 +1,340 @@
 <?php
-    namespace Omni;
+namespace Omnilance;
 
-    class Api
+class Api
+{
+
+    private $url = "https://api.omnilance.com";
+    private $api_version = '2.0';
+    private $apikey;
+
+    public function __construct($apikey, $url = Null, $version = Null)
     {
-		
-        private $url="https://api.omnilance.com";
-	private $apikey;
-	private $data;
-		
-	public function __construct($apikey,$url=null)
-	{
-	    $this->apikey = $apikey;
-	    if (!empty($url))
-	        $this->url = $url;
-	}
+        $this->apikey = $apikey;
+        if (!empty($url))
+            $this->url = $url;
 
-	private function Request()
-	{
-	    $this->data["apikey"] =$this->apikey;		
-	    $defaults = Array(
-	        CURLOPT_URL => $this->url,
-		CURLOPT_HEADER => 0,
-		CURLOPT_RETURNTRANSFER => true,
-		CURLOPT_SSL_VERIFYPEER=>0,
-		CURLOPT_SSL_VERIFYHOST=>0,
-		CURLOPT_TIMEOUT => 30,
-		CURLOPT_POST=>true,
-		CURLOPT_POSTFIELDS=>json_encode($this->data),	
-	    );
-	    $ch = curl_init();	
-	    curl_setopt_array($ch,$defaults);
-	    if (!$result = curl_exec($ch)) {
-		trigger_error(curl_error($ch));
-	    }
-	    curl_close($ch);
-	    $res = json_decode($result);
-	    return $res;
-	}
-		
-	public function CustomerAdd($customer)
-	{
-	    $this->data = Array(
-		"type" => "customer",
-		"comand" => "customer_add",
-		"data" => $customer	
-	    );
-	    $response = $this->Request();
-	    return $response;
-	}
+        if (!empty($version))
+            $this->api_version = $version;
+    }
 
-        public function CustomerUpdate($customer)
-	{
-	    $this->data = Array(
-	        "type" => "customer",
-		"comand" => "customer_update",
-		"data" => $customer	
-	    );
-	    $response = $this->Request();
-	    return $response;
-	}
+    /**
+     * @param $cmd
+     * @param array $data
+     * @param string $request_type
+     * @return mixed
+     */
+    private function Request($cmd, $data = [], $request_type = 'GET')
+    {
+        $url = implode('/', [$this->url, "v" . $this->api_version, $cmd]);
 
-	public function CustomerInfo($customer_id)
-	{
-	    $this->data = Array(
-	        "type" => "customer",
-		"comand" => "customer_info",
-		"data" => $customer	
-	    );
-	    $response = $this->Request();
-	    return $response;
-	}
+        $defaults = [
+            CURLOPT_URL => $url,
+            CURLOPT_HEADER => 0,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_SSL_VERIFYPEER => 0,
+            CURLOPT_SSL_VERIFYHOST => 0,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_CUSTOMREQUEST => $request_type,
+            CURLOPT_POSTFIELDS => http_build_query($data),
+            CURLOPT_HTTPHEADER => ["X-Token: {$this->apikey}"]
+        ];
 
-	public function CustomerDelete($customer_id)
-	{
-	    $this->data = Array(
-	        "type" => "customer",
-		"comand" => "customer_delete",
-		"data" => Array(
-		    "id" => $customer_id
-		),	
-	    );
-	    $response = $this->Request();
-	    return $response;
-	}
-	
-	public function DomainCheck($domainListForCheck)
-	{
-	    $this->data = Array(
-		"type" => "domain",
-		"comand" => "domain_check",
-		"data" => $domainListForCheck
-	    );
-	    $response = $this->Request();
-	    return $response;
-	}
+        $ch = curl_init();
+        curl_setopt_array($ch, $defaults);
+        if (!$result = curl_exec($ch)) {
+            trigger_error(curl_error($ch));
+        }
+        curl_close($ch);
 
-	public function DomainAdd($domain)
-	{
-	    $this->data = Array(
-		"type" => "domain",
-		"comand" => "domain_add",
-		"data" => Array($domain),
-	    );
-	    $response = $this->Request();
-	    return $response;
-	}
+        $response = json_decode($result);
+        return $response;
+    }
 
-	public function DomainUpdate($domain)
-	{
-	    $this->data = Array(
-	    	"type" => "domain",
-	    	"comand" => "domain_update",
-	    	"data" => $domain,
-	    );
-	    $response = $this->Request();
-	    return $response;
-	}
+    /**
+     * @param $customer_data
+     * @return mixed
+     */
+    public function CustomerAdd($customer_data)
+    {
+        $response = $this->Request("customer", $customer_data, "POST");
+        return $response;
+    }
 
-	public function DomainInfo($domainNameArray)
-	{
-	    $this->data = Array(
-		"type" => "domain",
-		"comand" => "domain_info",
-		"data" => $domainNameArray,
-	    );
-	    $response = $this->Request();
-	    return $response;
-	}
+    /**
+     * @param $customer_id
+     * @param $data
+     * @return mixed
+     */
+    public function CustomerUpdate($customer_id, $data)
+    {
+        $response = $this->Request("customer/$customer_id", $data, 'PUT');
+        return $response;
+    }
 
-	public function DomainRenew($domainInfo)
-	{
-	    $this->data = Array(
-		"type" => "domain",
-		"comand" => "domain_renew",
-		"data" => Array($domainInfo),
-	    );
-	    $response = $this->Request();
-	    return $response;
-	}
+    /**
+     * @param $customer_id
+     * @return mixed
+     */
+    public function CustomerInfo($customer_id)
+    {
+        $response = $this->Request("customer/$customer_id");
+        return $response;
+    }
 
-	public function DomainTransferRequest($domainInfo)
-	{
-	    $this->data = Array(
-		"type" => "domain",
-		"comand" => "domain_transfer_request",
-		"data" => $domainInfo
-	    );
-	    $response = $this->Request();
-	    return $response;
-	}
+    /**
+     * @param $customer_id
+     * @return mixed
+     */
+    public function CustomerDelete($customer_id)
+    {
+        $response = $this->Request("customer/$customer_id", [], "DELETE");
+        return $response;
+    }
 
-	public function DomainTransferQuery($domainInfo)
-	{
-	    $this->data = Array(
-		"type" => "domain",
-		"comand" => "domain_transfer_query",
-		"data" => $domainInfo
-	    );
-	    $response = $this->Request();
-	    return $response;
-	}
+    /**
+     * @param $domain
+     * @return mixed
+     */
+    public function DomainCheck($domain)
+    {
+        $response = $this->Request("domain/$domain/check");
+        return $response;
+    }
 
-	public function DomainTransferCancel($domainInfo)
-	{
-	   $this->data = Array(
-		"type" => "domain",
-		"comand" => "domain_transfer_cancel",
-		"data" => $domainInfo
-	    );
-	    $response = $this->Request();
-	    return $response;
-	}
+    /**
+     * @param $domain
+     * @param $domain_info
+     * @return mixed
+     */
+    public function DomainAdd($domain, $domain_info)
+    {
+        $response = $this->Request("domain/$domain", $domain_info, 'POST');
+        return $response;
+    }
 
-	public function DomainTransferApprove($domainInfo)
-	{
-	    $this->data = Array(
-		"type" => "domain",
-		"comand" => "domain_transfer_approve",
-		"data" => $domainInfo
-	    );
-	    $response = $this->Request();
-	    return $response;
-	}
+    /**
+     * @param $domain
+     * @param array $data
+     * @return mixed
+     */
+    public function DomainUpdate($domain, $data = [])
+    {
+        $response = $this->Request("domain/$domain", $data, 'PUT');
+        return $response;
+    }
 
-	public function DomainTransferReject($domainInfo)
-	{
-	    $this->data = Array(
-		"type" => "domain",
-		"comand" => "domain_transfer_reject",
-		"data" => $domainInfo
-	    );
-	    $response = $this->Request();
-	    return $response;
-	}
+    /**
+     * @param $domain
+     * @return mixed
+     */
+    public function DomainInfo($domain)
+    {
+        $response = $this->Request("domain/$domain");
+        return $response;
+    }
 
-	public function DomainRestore($domainName)
-	{
-	    $this->data = Array(
-		"type" => "domain",
-		"comand" => "domain_restore",
-		"data" => Array(
-		    "name" => $domainName
-		)
-	    );
-	    $response = $this->Request();
-	    return $response;
-	}
+    /**
+     * @param $domain
+     * @param array $data
+     * @return mixed
+     */
+    public function DomainRenew($domain, $data = [])
+    {
+        $response = $this->Request("domain/$domain" . '/renew', $data, 'PUT');
+        return $response;
+    }
 
-	public function DomainDelete($domainName)
-	{
-	    $this->data = Array(
-		"type" => "domain",
-		"comand" => "domain_delete",
-		"data" => Array(
-		    "name" => $domainName
-		)
-	    );
-	    $response = $this->Request();
-	    return $response;
-	}
+    /*
+    // Start Transfer
+    */
+    /**
+     * @param $domain
+     * @param array $data
+     * @return mixed
+     */
+    public function DomainTransferRequest($domain, $data = [])
+    {
+        $response = $this->Request("domain/$domain" . '/transfer', $data, 'POST');
+        return $response;
+    }
 
-	public function ContactCheck($contact_id)
-	{
-	    $this->data = Array(
-		"type" => "contact",
-		"comand" => "contact_check",
-		"data" => Array(
-		    "contact_id" => $contact_id
-		),
-	     );
-	    $response = $this->Request();
-	    return $response;
-	}
+    /**
+     * @param $domain
+     * @param array $data
+     * @return mixed
+     */
+    public function DomainTransferQuery($domain, $data = [])
+    {
+        $response = $this->Request("domain/$domain" . '/transfer', $data, 'GET');
+        return $response;
+    }
 
-	public function ContactAdd($contact)
-	{
-	    $this->data = Array(
-		"type" => "contact",
-		"comand" => "contact_add",
-		"data" => $contact
-	    );
-	    $response = $this->Request();
-	    return $response;
-	}
+    /**
+     * @param $domain
+     * @param array $data
+     * @return mixed
+     */
+    public function DomainTransferCancel($domain, $data = [])
+    {
+        $response = $this->Request("domain/$domain" . '/transfer', 'DELETE');
+        return $response;
+    }
 
-	public function ContactInfo($contactIdList)
-	{
-	    $this->data=  Array(
-		"type" => "contact",
-		"comand" => "contact_info",
-		    "data"=>$contactNameList
-	    );
-	    $response = $this->Request();
-	    return $response;
-	}
+    /**
+     * @param $domain
+     * @param array $data
+     * @return mixed
+     */
+    public function DomainTransferApprove($domain, $data = [])
+    {
+        $response = $this->Request("domain/$domain" . '/transfer/approve', 'PUT');
+        return $response;
+    }
 
-	public function ContactUpdate($contactInfo)
-	{
-	    $this->data = Array(
-		"type" => "contact",
-		"comand" => "contact_update",
-		"data" => $contactInfo
-	    );
-	    $response = $this->Request();
-	    return $response;
-	}
+    /**
+     * @param $domain
+     * @param array $data
+     * @return mixed
+     */
+    public function DomainTransferReject($domain, $data = [])
+    {
+        $response = $this->Request("domain/$domain" . '/transfer/reject', 'PUT');
+        return $response;
+    }
 
-	public function ContactDelete($contactId)
-	{
-	     $this->data = Array(
-		"type" => "contact",
-		"comand" => "contact_delete",
-		"data" => Array(
-		    "contact_id" => $contactName
-		),
-	     );
-	     $response = $this->Request();
-	     return $response;
-	}
+    /**
+     * @param $domain
+     * @param array $data
+     * @return mixed
+     */
+    public function DomainRestore($domain, $data = [])
+    {
+        $response = $this->Request("domain/$domain" . '/restore', $data, 'PUT');
+        return $response;
+    }
 
-	public function HostCheck($hostNameArray,$domainZone)
-	{
-	     $this->data = Array(
-		"type" => "host",
-		"comand" => "host_check",
-		"data" => Array(
-		    "host" => $hostNameArray,
-		     "zone"=>$domainZone,
-		)
-	     );
-	     $response = $this->Request();
-	     return $response;
-	}
-		
-	public function HostAdd($hostInfo,$domain)
-	{
-	     $this->data = Array(
-	 	"type" => "host",
-		"comand" => "host_add",
-		"data" => Array(
-		    "host" => $hostInfo,
-		    "domain" => $domain
-		)
-	     );
-	     $response = $this->Request();
-	     return $response;
-	}
+    /**
+     * @param $domain
+     * @param array $data
+     * @return mixed
+     */
+    public function DomainDelete($domain, $data = [])
+    {
+        $response = $this->Request("domain/$domain",  $data, 'DELETE');
 
-	public function HostInfo($hostName,$domainName)
-	{
-	    $this->data = Array(
-		"type" => "host",
-		"comand" => "host_info",
-		"data" => Array(
-		     "host" => $hostName,
-		     "domain" => $domainName
-		)
-	    );
-	    $response = $this->Request();
-	    return $response;
-	}
+        return $response;
+    }
 
-	public function HostDelete($hostName,$domainName)
-	{
-	    $this->data = Array(
-		"type" => "host",
-		"comand" => "host_delete",
-		"data" => Array(
-		    "host" => $hostName,
-		    "domain" => $domainName,
-		),
-	    );
-	    $response = $this->Request();
-	    return $response;
-	}
+    /**
+     * @param $contact_id
+     * @return mixed
+     */
+    public function ContactCheck($contact_id)
+    {
+        $response = $this->Request("contact/$contact_id/check");
+        return $response;
+    }
 
-	public function HostUpdate($hostInfo, $domainName)
-	{
-	     $this->data = Array(
-		"type" => "host",
-		"comand" => "host_update",
-		"data" => Array(
-		     "host" => $hostInfo,
-		     "domain" => $domainName
-		),
-	     );
-	     $response = $this->Request();
-	     return $response;
-	}
+    /**
+     * @param $contact_id
+     * @param $contact_info
+     * @return mixed
+     */
+    public function ContactAdd($contact_id, $contact_info)
+    {
+        $response = $this->Request("contact/$contact_id", $contact_info, 'POST');
+        return $response;
+    }
+
+    /**
+     * @param $contact_id
+     * @return mixed
+     */
+    public function ContactInfo($contact_id)
+    {
+        $response = $this->Request("contact/$contact_id");
+        return $response;
+    }
+
+    /**
+     * @param $contact_id
+     * @param array $data
+     * @return mixed
+     */
+    public function ContactUpdate($contact_id, $data = [])
+    {
+        $response = $this->Request("contact/$contact_id", $data, 'PUT');
+        return $response;
+    }
+
+    /**
+     * @param $contact_id
+     * @return mixed
+     */
+    public function ContactDelete($contact_id)
+    {
+        $response =  $this->Request("contact/$contact_id",[],'DELETE');
+        return $response;
+    }
+
+    /**
+     * @param $hostName
+     * @param $domain
+     * @param array $data
+     * @return mixed
+     */
+    public function HostCheck($hostName, $domain, $data = [])
+    {
+        $response =  $this->Request("domain/$domain" . '/host/' . $hostName . '/check', $data);
+        return $response;
+    }
+
+    /**
+     * @param $hostName
+     * @param $domain
+     * @param array $data
+     * @return mixed
+     */
+    public function HostAdd($hostName, $domain, $data = [])
+    {
+        $response =  $this->Request("domain/$domain" . '/host/' . $hostName, $data, 'POST');
+        return $response;
+    }
+
+    /**
+     * @param $hostName
+     * @param $domain
+     * @param array $data
+     * @return mixed
+     */
+    public function HostInfo($hostName, $domain, $data = [])
+    {
+        $response =  $this->Request("domain/$domain" . '/host/' . $hostName, $data);
+        return $response;
+    }
+
+    /**
+     * @param $hostName
+     * @param $domain
+     * @param array $data
+     * @return mixed
+     */
+    public function HostDelete($hostName, $domain, $data = [])
+    {
+        $response =  $this->Request("domain/$domain" . '/host/' . $hostName, $data, 'DELETE');
+        return $response;
+    }
+
+    /**
+     * @param $hostName
+     * @param $domain
+     * @param array $data
+     * @return mixed
+     */
+    public function HostUpdate($hostName, $domain, $data = [])
+    {
+        $response = $this->Request("domain/$domain" . '/host/'. $hostName, $data, 'PUT');
+        return $response;
+    }
 }
